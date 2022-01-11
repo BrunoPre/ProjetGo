39084b8f7b18aa71670bb8814288155082e92511
Bruno Preka <bruno.preka@imt-atlantique.net>
2022-01-11 17:30:51 +0100

    By Pierre: another way to generate data thx to trigonometry

--- pkg/mqtt/structs/sensors.go
+++ pkg/mqtt/structs/sensors.go
@@ -2,9 +2,7 @@ package structs
 
 import (
 	"fmt"
-	"github.com/aquilax/go-perlin"
 	"math"
-	"math/rand"
 	"time"
 )
 
@@ -24,13 +22,34 @@ type SensorData struct {
 }
 
 func (s Sensor) GenerateData(currentTime time.Time) SensorData {
-	p := perlin.NewPerlinRandSource(2, 2, 2, rand.NewSource(100))
-	valRand := math.Abs(p.Noise1D(float64(currentTime.Unix())))
-	valRand = valRand / 1000000000000000000000000000000000000 // TODO: find a proper & cleaner way to get a 2-digit integer
-	//valRand := GenerateStableRandomNumericalValues(s.Measure)
-	return SensorData{s.Id, s.AirportId, s.Measure, float64(valRand), currentTime}
+	/*
+		paramValues := map[Measure][3]float64{
+			Temperature: {2, 2, 2},
+			Pressure:    {2, 2, 2},
+			Wind:        {2, 2, 2},
+		}
+		param := paramValues[s.Measure]
+		p := perlin.NewPerlinRandSource(param[0], param[1], int32(param[2]), rand.NewSource(100))
+		valRand := math.Abs(p.Noise1D(float64(currentTime.Unix())))
+		valRand = valRand / 1000000000000000000000000000000000000 // TODO: find a proper & cleaner way to get a 2-digit integer
+		//valRand := GenerateStableRandomNumericalValues(s.Measure)
+
+	*/
+	val := GenData(currentTime)
+	return SensorData{s.Id, s.AirportId, s.Measure, val, currentTime}
 }
 
 func (s SensorData) String() string {
 	return fmt.Sprintf("{Id=%d; AirportId=%s; Measure=%s; Value=%f; TimeStamp=%s}", s.Id, s.AirportId, s.Measure, s.Value, s.Timestamp.String())
+}
+
+func GenData(currentTime time.Time) float64 {
+	firstTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, currentTime.Location())
+	x := currentTime.Sub(firstTime)
+	return CourbeTemperature(x.Seconds())
+}
+
+func CourbeTemperature(x float64) float64 {
+	x = x / 60
+	return 3.5*math.Cos(math.Pi/12*x+2.7) + 0.02*math.Cos(4*math.Pi*x) + 0.5*math.Cos(0.5*math.Pi*x) + 8
 }
